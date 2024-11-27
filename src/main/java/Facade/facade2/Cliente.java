59d4e737704de92e00850ed46e1244a8f285d249
ADRIANO ANTONIO NOGUEIRA DE PAULA <adriano.paula@GRANBERY.local>
2024-11-27 20:34:04 -0300

    atualizado3

--- src/main/java/Facade/facade2/Cliente.java
+++ src/main/java/Facade/facade2/Cliente.java
@@ -3,20 +3,11 @@ package Facade.facade2;
 public class Cliente {
     public static void main(String[] args) {
         Facade facade = new Facade();
-<<<<<<< HEAD
 
         String resultadoModoCinema = facade.modoCinema();
         String resultadoModoNormal = facade.modoNormal();
 
 
     }
-=======
-        System.out.println("Modo Cinema");
-        facade.modoCinema();
 
-        System.out.println("Modo Normal");
-        facade.modoNormal();
-    }
-
->>>>>>> fc9cec0c05ea089bed935a52937062d2a0f233fc
 }
