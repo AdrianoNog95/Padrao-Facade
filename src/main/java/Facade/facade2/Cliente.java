f9efe858dc88b0609c9f5e5bc97d8c095805a873
ADRIANO ANTONIO NOGUEIRA DE PAULA <adriano.paula@GRANBERY.local>
2024-11-27 20:09:30 -0300

    “atualizado1”

---
+++ src/main/java/Facade/facade2/Cliente.java
@@ -0,0 +1,12 @@
+package Facade.facade2;
+
+public class Cliente {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+
+        String resultadoModoCinema = facade.modoCinema();
+        String resultadoModoNormal = facade.modoNormal();
+
+
+    }
+}
